34e857e3dba1222e175208da03ad10f1e9df9311
akasergej <akasergej@gmail.com>
2021-07-01 21:22:38 +0300

    passing all tests, format document

--- src/Jurgis.php
+++ src/Jurgis.php
@@ -8,57 +8,54 @@ class Jurgis
 {
     public function responds(string $message): string
     {
-        if($this->checkGreeting($message)) {
+        if ($this->checkGreeting($message)) {
             return "Labas!";
         }
 
-        if($this->checkQuestion($message)) {
-            return "Okis.";
-        }
-
-        if($this->checkShout($message)) {
+        if ($this->checkShout($message)) {
             return "Oi oi, atvėsk!";
         }
 
-        if($this->checkSilence($message)) {
+        if ($this->checkQuestion($message)) {
+            return "Okis.";
+        }
+
+        if ($this->checkSilence($message)) {
             return "Alio! Kuku?";
         }
 
         return "Aha gerai.";
     }
 
-    private function checkGreeting(string $message) 
+    private function checkGreeting(string $message)
     {
-        if($message == "Sveiki" || $message == "LABAS") {
+        if ($message == "Sveiki" || $message == "LABAS") {
             return true;
         }
         return false;
     }
 
-
-    private function checkQuestion(string $message) 
+    private function checkQuestion(string $message)
     {
-        if(substr(trim($message), -1)=='?') {
+        if (substr(trim($message), -1) == '?') {
             return true;
         }
         return false;
     }
 
-    private function checkShout(string $message) 
+    private function checkShout(string $message)
     {
-        if(mb_strtoupper(trim($message))===trim($message) && preg_match("/[a-z]/i", trim($message)) > 0) {
+        if (mb_strtoupper(trim($message)) === trim($message) && preg_match("/[a-z]/i", trim($message)) > 0) {
             return true;
         }
         return false;
     }
 
-    private function checkSilence(string $message) 
+    private function checkSilence(string $message)
     {
-        if(trim($message) == '') {
+        if (trim($message) == '') {
             return true;
         }
         return false;
     }
-
-
 }
